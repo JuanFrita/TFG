bfa5ba5d8d9e783af1c174db880ea0ceca5ac39b
juanfran sanchez <erjuanfri01@gmail.com>
2023-03-08 16:35:57 +0100

    Separar videos en fotos

--- video_to_photo.py
+++ video_to_photo.py
@@ -3,6 +3,7 @@ import cv2
 video = cv2.VideoCapture('videos_playas/soleado_alto.mp4')
 # Get the number of frames
 num_frames = int(video.get(cv2.CAP_PROP_FRAME_COUNT))
+print(num_frames)
 # Loop through the frames and save each one as an image file
 for frame_num in range(num_frames):
     # Read the frame
@@ -10,7 +11,7 @@ for frame_num in range(num_frames):
     # Check if the frame was successfully read
     if ret:
         # Save the frame as an image file
-        cv2.imwrite(f'/fotos_playas/soleado_alto_{frame_num}.jpg', frame)
+        cv2.imwrite(f'soleado_alto_{frame_num}.jpg', frame)
     else:
         break
 # Release the video file
