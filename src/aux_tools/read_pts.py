d3f9410636264952014c5a45746eef5e1efc4ffa
juanfran sanchez <erjuanfri01@gmail.com>
2023-03-12 19:02:46 +0100

    Mejorar estructura del proyecto

--- src/aux_tools/read_pts.py
+++ src/aux_tools/read_pts.py
@@ -4,6 +4,6 @@ def read_pts(filename):
     return np.loadtxt(filename, comments=("version:", "n_points:", "{", "}")).astype(np.float32)
 
 # Example usage
-filename = 'D:\TFG\src\example.pts'
+filename = 'D:/TFG/src/resources/pts_examples/example.pts'
 points = read_pts(filename)
 print(points)
